b64ce767addd22e559561917976074deaa2d53c0
Sivan Mehta <Sivanmehta2@gmail.com>
2017-04-15 13:43:55 -0400

    color each square by active or not

--- conway.js
+++ conway.js
@@ -6,10 +6,13 @@ function gridData(rows = 10, cols = 10, width = 100, height = 100) {
     data.push( new Array() )
     for (var col = 0; col < cols; col++) {
       data[row].push({
-        x: col * cellWidth + 1,
-        y: row * cellHeight + 1,
+        row,
+        col,
+        posX: col * cellWidth + 1,
+        posY: row * cellHeight + 1,
         width: cellWidth,
-        height: cellHeight
+        height: cellHeight,
+        active: Math.random() > .5
       })
     }
   }
@@ -17,11 +20,11 @@ function gridData(rows = 10, cols = 10, width = 100, height = 100) {
 }
 
 // populate grid data
-const numRows = 25
-const numCols = 25
-const gridHeight = window.innerHeight - 20
-const gridWidth = window.innerWidth - 20
-var data = gridData(numRows, numCols, gridWidth, gridHeight)
+const numRows = 10
+const numCols = 10
+const gridHeight = 500
+const gridWidth = 500
+var gridData = gridData(numRows, numCols, gridWidth, gridHeight)
 
 // initialize grid
 var grid = d3.select("#life")
@@ -31,18 +34,51 @@ var grid = d3.select("#life")
 
 // create each row
 var row = grid.selectAll(".row")
-  .data(data)
+  .data(gridData)
   .enter().append("g")
   .attr("class", "row")
 
-// create each column
-var column = row.selectAll(".square")
-  .data(d => d)
-  .enter().append("rect")
-  .attr("class", "square")
-  .attr("x", d => d.x)
-  .attr("y", d => d.y)
-  .attr("width", d => d.width)
-  .attr("height", d => d.height)
-  .style("fill", "#fff")
-  .style("stroke", "#000")
+function fillSquares () {
+  // create each square
+  row.selectAll(".square")
+    .data(d => d)
+    .enter().append("rect")
+    .attr("class", "square")
+    .attr("x", d => d.posX)
+    .attr("y", d => d.posY)
+    .attr("width", d => d.width)
+    .attr("height", d => d.height)
+    .style("fill", "#fff")
+    .style("stroke-width", 0)
+
+  // color active cells
+  d3.selectAll(".square")
+    .filter((d, i) => d.active)
+    .style("fill", "#888")
+}
+
+fillSquares()
+
+function animate () {
+  // define directions
+  const dirs = [
+    [-1, -1], [0, -1], [1, -1],
+    [-1, 0],           [1, 0],
+    [-1, 1],  [0, 1],  [1, 1],
+  ]
+
+  function getNeighbors (x, y) {
+    return dirs.reduce((prev, dir) => {
+      var isActive = false
+      try {
+        isActive = isActive || gridData[x + dir[0]][y + dir[1]].active
+      } catch(e) { }
+      return prev + isActive
+    }, 0)
+  }
+
+  d3.selectAll(".square")
+    .attr("neighbors", d => getNeighbors(d.row, d.col))
+}
+
+animate()
