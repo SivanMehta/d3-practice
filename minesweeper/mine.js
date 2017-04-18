216fd89a3710bbd1ceb274675209671477236701
Sivan Mehta <Sivanmehta2@gmail.com>
2017-04-18 18:56:53 -0400

    stopped using classes

--- minesweeper/mine.js
+++ minesweeper/mine.js
@@ -1,69 +1,12 @@
-class Minesweeper {
-  constructor(width, height, mines) {
-    this.width = width
-    this.height = height
-    this.mines = mines
-
-    this.grid = d3.select("#sweep")
-      .append("svg")
-      .attr("width", (window.innerWidth + 10) + "px")
-      .attr("height", (window.innerHeight + 10) + "px")
-      .attr("class", "minesweeper")
-  }
-
-  render() {
-    const cellWidth = (window.innerWidth - 20) / this.width
-
-    this.grid.selectAll('.cell')
-      .data(this.cells).enter().append('rect')
-      .attr('class', 'cell')
-      .attr('x', c => c.col * cellWidth)
-      .attr('y', c => c.row * cellWidth)
-      .attr('width', cellWidth)
-      .attr('height', cellWidth)
-      .style('fill', "#000")
-      .style('stroke', '#000')
-      .on('click', function (cell) {
-        console.log(cell)
-
-        var color;
-        if(cell.value < 0) {
-          color = "#f00"
-          alert("You suck!")
-          return
-        } else {
-          color = getColor(cell.value)
-          score += 1
-        }
-        color = cell.value == 0 ? "#fff" : color
-
-        // color in the cell and remove click handler
-        d3.select(this)
-          .style("fill", color)
-          .on('click', null)
-        // add the appropriate text
-        d3.select("#sweep svg").append('text')
-          .attr('x', _ => cell.col * cellWidth + cellWidth * .5)
-          .attr('y', _ => cell.row * cellWidth + cellWidth * .5)
-          .attr('text-anchor', 'middle')
-          .attr('fill', '#000')
-          .text(_ => cell.value)
-        // update the score
-        d3.select("#score")
-          .text(score)
-      })
-  }
-}
-
+// setup grid
 const width = 20
 const height = 10
-mines = 30
+const mines = 30
 
-var game = new Minesweeper(width, height, 5)
-game.cells = Array()
-
+// populate cell grid
+var cells = Array()
 for(var i = 0; i < width * height; i ++) {
-  game.cells.push({
+  cells.push({
     row: Math.floor(i / width),
     col: i % width,
     value: 0,
@@ -72,12 +15,11 @@ for(var i = 0; i < width * height; i ++) {
   })
 }
 
-var mineIncedes = [...Array(width*height).keys()]
+var mineIncedes = [...Array(width * height).keys()]
 mineIncedes = mineIncedes.sort((a, b) => (0.5 - Math.random()))
 mineIncedes = mineIncedes.slice(0, mines)
-
 mineIncedes.forEach(index => {
-  game.cells[index].value = -1
+  cells[index].value = -1
 })
 
 function surroundingBombs (cell, i, board) {
@@ -103,12 +45,58 @@ function surroundingBombs (cell, i, board) {
   return bombs
 }
 
+const bombCounts = cells.map(surroundingBombs)
+cells.forEach((cell, i) => {
+  cell.value = cell.value >= 0 ? bombCounts[i] : cell.value
+})
+
 function getColor(value) {
   return "rgb(0,"+ (value * 32) + ",0)"
 }
 
-const bombCounts = game.cells.map(surroundingBombs)
-game.cells.forEach((cell, i) => {
-  cell.value = cell.value >= 0 ? bombCounts[i] : cell.value
-})
-game.render()
+// render grid
+var grid = d3.select("#sweep")
+  .append("svg")
+  .attr("width", (window.innerWidth + 10) + "px")
+  .attr("height", (window.innerHeight + 10) + "px")
+  .attr("class", "minesweeper")
+const cellWidth = (window.innerWidth - 20) / width
+
+grid.selectAll('.cell')
+  .data(cells).enter().append('rect')
+  .attr('class', 'cell')
+  .attr('x', c => c.col * cellWidth)
+  .attr('y', c => c.row * cellWidth)
+  .attr('width', cellWidth)
+  .attr('height', cellWidth)
+  .style('fill', "#000")
+  .style('stroke', '#000')
+  .on('click', function (cell) {
+    console.log(cell)
+
+    var color;
+    if(cell.value < 0) {
+      color = "#f00"
+      alert("You suck!")
+      return
+    } else {
+      color = getColor(cell.value)
+      score += 1
+    }
+    color = cell.value == 0 ? "#fff" : color
+
+    // color in the cell and remove click handler
+    d3.select(this)
+      .style("fill", color)
+      .on('click', null)
+    // add the appropriate text
+    d3.select("#sweep svg").append('text')
+      .attr('x', _ => cell.col * cellWidth + cellWidth * .5)
+      .attr('y', _ => cell.row * cellWidth + cellWidth * .5)
+      .attr('text-anchor', 'middle')
+      .attr('fill', '#000')
+      .text(_ => cell.value)
+    // update the score
+    d3.select("#score")
+      .text(score)
+  })
