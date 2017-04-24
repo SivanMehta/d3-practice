4221f7e78770b4a6b5f9eb0537bf367ecdcb3bc9
Sivan Mehta <Sivanmehta2@gmail.com>
2017-04-23 21:11:32 -0400

    read data from csv file

--- swimming/swim.js
+++ swimming/swim.js
@@ -1,2 +1,28 @@
-d3.select('#graph')
-  .text('hello!')
+const margin = {top: 20, right: 20, bottom: 30, left: 40}
+const width = window.innerWidth - margin.left - margin.right
+const height = window.innerHeight - margin.top - margin.bottom
+
+var x = d3.scaleLinear()
+  .range([0, width])
+
+var y = d3.scaleLinear()
+  .range([height, 0])
+
+var yAxis = d3.axisLeft(y)
+var xAxis = d3.axisBottom(x)
+
+var svg = d3.select('#graph').append('svg')
+  .attr('width', width + margin.left + margin.right)
+  .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+
+var psv = d3.dsvFormat("|")
+d3.request("/data.csv")
+  .mimeType("text/plain")
+  .response(xhr => psv.parse(xhr.responseText))
+  .get(renderData)
+
+function renderData(data) {
+  console.log(data[0])
+}
