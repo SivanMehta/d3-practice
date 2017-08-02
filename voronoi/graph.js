8684c765c46072398fd5f9b3a54915a3576262b3
Sivan Mehta <Sivanmehta2@gmail.com>
2017-08-01 17:41:58 -0700

    draw voronoi diagram on top of data

--- voronoi/graph.js
+++ voronoi/graph.js
@@ -1,27 +1,3 @@
-// Neural Net
-
-var layer_defs = [];
-layer_defs.push({type:'input', out_sx:1, out_sy:1, out_depth:2});
-layer_defs.push({type:'fc', num_neurons:6, activation: 'tanh'});
-layer_defs.push({type:'fc', num_neurons:4, activation: 'tanh'});
-layer_defs.push({type:'fc', num_neurons:2, activation: 'tanh'});
-layer_defs.push({type:'softmax', num_classes:2});
-
-var net = new convnetjs.Net();
-net.makeLayers(layer_defs);
-
-var trainer = new convnetjs.SGDTrainer(net, {learning_rate:0.01, momentum:0.1, batch_size:10, l2_decay:0.001});
-
-var iteration = 0
-function train(data) {
-  var x = new convnetjs.Vol(1,1,2);
-  data.forEach(row => {
-    x.w = [row.x, row.y]
-    trainer.train(x, row.label)
-  })
-
-  generation.text("Generation "+ ++iteration)
-}
 
 // Graphing
 
@@ -36,6 +12,10 @@ var x = d3.scaleLinear()
 var y = d3.scaleLinear()
   .domain([.5, 2])
   .range([height, 0])
+
+
+var voronoi = d3.voronoi()
+  .extent([[-1, -1], [width + 1, height + 1]])
 
 var color = (col) => col == 1 ? '#f00' : '#0f0'
 var label = (col) => col == 'upper' ? 1 : 0
@@ -78,14 +58,8 @@ function showTooltip(pt) {
     .attr('class', 'highlight')
 }
 
-function guessPoint() {
-  d3.event.stopPropagation();
-  var coords = d3.mouse(this)
-  coords = [x.invert(coords[0]), y.invert(coords[1])]
-  const guess = new convnetjs.Vol(coords)
-
-  var probability_volume = net.forward(guess);
-  console.log(probability_volume.w);
+function redrawPolygon(polygon) {
+  polygon.attr("d", function(d) { return d ? "M" + d.join("L") + "Z" : null; });
 }
 
 d3.csv("./boomerang.csv", (err, data) => {
@@ -94,6 +68,15 @@ d3.csv("./boomerang.csv", (err, data) => {
     row.y = +row.y
     row.label = label(row.label)
   })
+
+  var polygonData = data.map(row => [x(row.x), y(row.y)])
+
+  var polygon = svg.append("g")
+    .attr("class", "polygons")
+    .selectAll("path")
+    .data(voronoi.polygons(polygonData))
+    .enter().append("path")
+      .call(redrawPolygon);
 
   svg.selectAll('dot')
     .data(data).enter()
@@ -113,8 +96,4 @@ d3.csv("./boomerang.csv", (err, data) => {
   svg.append("g")
     .attr("class", "y axis")
     .call(yAxis)
-
-  d3.select('#graph').on('click', guessPoint)
-
-  setInterval(train, 100, data)
 })
